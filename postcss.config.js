f496e8162d383f778021312549fc309fec1e512b
bl-rd <me@bl-rd.me>
2021-11-15 21:35:12 +0000

    Get css hot reloading working

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  // input: 'src/assets/styles/main.css',
+  // output: 'src/assets/styles/compiled.css',
+  // verbose: true,
   plugins: [
     require('postcss-import')()
   ]
